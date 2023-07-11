ffde8cae2bbb4caf00c465f1c288613ca9a7bad2
Dishie2498 <vdishie@gmail.com>
2023-07-11 15:05:30 +0530

    abs path ..

--- docs/conf.py
+++ docs/conf.py
@@ -9,8 +9,8 @@
 import os
 import sys
 
-# sys.path.insert(0, os.path.abspath(".."))
-sys.path.append(os.path.abspath("sphinxext"))
+sys.path.insert(0, os.path.abspath(".."))
+# sys.path.append(os.path.abspath("sphinxext"))
 
 project = "HOI"
 copyright = "BraiNets"

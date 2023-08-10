3092d69b89b39f6be5b4f644262193d11baa1871
Dishie2498 <vdishie@gmail.com>
2023-08-11 00:13:31 +0530

    solving errors

--- hoi/core/tests/test_entropies.py
+++ hoi/core/tests/test_entropies.py
@@ -2,7 +2,12 @@ import pytest
 import numpy as np
 import jax
 
-from hoi.core.entropies import entropy_gcmi, entropy_bin, entropy_knn, entropy_kernel
+from hoi.core.entropies import (
+    entropy_gcmi,
+    entropy_bin,
+    entropy_knn,
+    entropy_kernel,
+)
 from hoi.utils import digitize
 
 x1 = np.random.rand(1, 50)
@@ -19,32 +24,42 @@ j2 = jax.random.uniform(jax.random.PRNGKey(0), shape=(10, 50))
 @pytest.mark.parametrize("demean", [True, False])
 def test_entropy_gcmi(x, biascorrect, demean):
     hx = entropy_gcmi(x, biascorrect, demean)
-    assert isinstance(hx, float)
+    hx = np.asarray(hx)
+    assert hx.dtype == np.float32
     pass
 
 
 @pytest.mark.parametrize("x", [x1, x2, j1, j2, l1, l2])
-@pytest.mark.parametrize("base", [np.random.randint(1, 100) for _ in range(10)])
+@pytest.mark.parametrize(
+    "base", [np.random.randint(1, 100) for _ in range(10)]
+)
 def test_entropy_bin(x, base):
     x_bin = digitize(x, n_bins=3)
     hx = entropy_bin(x_bin, base)
-    assert isinstance(hx, float)
+    hx = np.asarray(hx)
+    assert hx.dtype == np.float32
     pass
 
 
 @pytest.mark.parametrize("x", [x1, x2, j1, j2, l1, l2])
-@pytest.mark.parametrize("closest", [np.random.randint(1, 100) for _ in range(10)])
+@pytest.mark.parametrize(
+    "closest", [np.random.randint(1, 100) for _ in range(10)]
+)
 def test_entropy_knn(x, closest):
     hx = entropy_knn(x, closest)
-    assert isinstance(hx, float)
+    hx = np.asarray(hx)
+    assert hx.dtype == np.float32
     pass
 
 
 @pytest.mark.parametrize("x", [x1, x2, j1, j2, l1, l2])
-@pytest.mark.parametrize("base", [np.random.randint(1, 100) for _ in range(10)])
+@pytest.mark.parametrize(
+    "base", [np.random.randint(1, 100) for _ in range(10)]
+)
 def test_entropy_kernel(x, base):
     hx = entropy_kernel(x, base)
-    assert isinstance(hx, float)
+    hx = np.asarray(hx)
+    assert hx.dtype == np.float32
 
 
 # tests core/entropies/copnorm_nd.py

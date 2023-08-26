f856d20f2f9e277eccc87a14d65fe17009284b94
Dishie2498 <vdishie@gmail.com>
2023-08-26 11:05:54 +0530

    class

--- hoi/core/tests/test_combinatory.py
+++ hoi/core/tests/test_combinatory.py
@@ -8,24 +8,26 @@ import jax.numpy as jnp
 from collections.abc import Iterable
 import itertools
 
-@pytest.mark.parametrize("n", [np.random.randint(5, 10) for _ in range(10)])
-@pytest.mark.parametrize("k", [np.random.randint(5, 10) for _ in range(10)])
-@pytest.mark.parametrize("order", [True, False])
-def test_single_combinations(n, k, order):
-    c = list(_combinations(n, k, order))
-    assert len(c) == ccomb(n, k)
-    pass
 
+class TestCombinatory(object):
 
-@pytest.mark.parametrize("n", [np.random.randint(5, 10) for _ in range(2)])
-@pytest.mark.parametrize("min", [np.random.randint(1, 10) for _ in range(2)])
-@pytest.mark.parametrize("max", [_ for _ in range(2)])  # addition to minimum size
-@pytest.mark.parametrize("astype", ["numpy", "jax", "iterator"])
-@pytest.mark.parametrize("order_val", [True, False])
-def test_combinations(n, min, max, astype, order_val):
-    combs = combinations(n, min, min + max, astype, order_val)
-    assert isinstance(combs, Iterable)
-    pass
+    @pytest.mark.parametrize("n", [np.random.randint(5, 10) for _ in range(10)])
+    @pytest.mark.parametrize("k", [np.random.randint(5, 10) for _ in range(10)])
+    @pytest.mark.parametrize("order", [True, False])
+    def test_single_combinations(self, n, k, order):
+        c = list(_combinations(n, k, order))
+        assert len(c) == ccomb(n, k)
+        pass
+
+    @pytest.mark.parametrize("n", [np.random.randint(5, 10) for _ in range(2)])
+    @pytest.mark.parametrize("min", [np.random.randint(1, 10) for _ in range(2)])
+    @pytest.mark.parametrize("max", [_ for _ in range(2)])  # addition to minimum size
+    @pytest.mark.parametrize("astype", ["numpy", "jax", "iterator"])
+    @pytest.mark.parametrize("order_val", [True, False])
+    def test_combinations(n, min, max, astype, order_val):
+        combs = combinations(n, min, min + max, astype, order_val)
+        assert isinstance(combs, Iterable)
+        pass
 
 
 # # # def isiterable(x):
